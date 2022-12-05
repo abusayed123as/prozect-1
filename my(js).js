9fe4dd2f2f2bc7f1abf53123625e20f20dd9712e
abusayed <abusayed@yandex.com>
2022-12-05 22:02:32 +0600

    my new site is ready

---
+++ my(js).js
@@ -0,0 +1,24 @@
+const man1=document.querySelector("#li1");
+man1.addEventListener("click",function () {
+   location.assign("'https://web.facebook.com/profile.php?id=100049015728694")
+});
+
+const man2=document.querySelector("#li2");
+man2.addEventListener("click",function () {
+   location.assign("https://www.youtube.com/")
+});
+
+const man3=document.querySelector("#li3");
+man3.addEventListener("click",function () {
+   location.assign("https://www.instagram.com/")
+});
+
+const man4=document.querySelector("#li4");
+man4.addEventListener("click",function () {
+   location.assign("https://twitter.com/i/flow/login")
+});
+
+const man5=document.querySelector("#li5");
+man5.addEventListener("click",function () {
+   location.assign("https://bd.linkedin.com/")
+});
